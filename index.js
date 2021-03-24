73e58e27f739632771ac02a5545fad6c3fd46d81
dewarian <nathanbommezijn@gmail.com>
2021-03-24 10:08:24 +0100

    :construction: Setup server
    Code is mostly copy for now

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", async (req, res) => {
+  res.render("index", {
+    pageTitle: "Minor Web Survey",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
