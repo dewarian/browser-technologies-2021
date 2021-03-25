7317d682e00203547e944fd4a97626fe0b8174de
dewarian <nathanbommezijn@gmail.com>
2021-03-25 14:12:27 +0100

    :art: Improve structure code, routers
    Routes are moved to their own file for cleaner code

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
+const router = require("./routes/router");
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", async (req, res) => {
-  res.render("index", {
-    pageTitle: "Minor Web Survey",
-  });
-});
+app.get("/", router);
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
