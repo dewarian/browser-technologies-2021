75f5b468a9e9a75d5dcd3c397320ee66e9201770
dewarian <nathanbommezijn@gmail.com>
2021-03-25 16:02:27 +0100

    :poop: Attempt on making the flash disappear

--- public/script/index.js
+++ public/script/index.js
@@ -6,6 +6,7 @@ const noJavascript = document.querySelector("label");
 
 if (noJavascript.classList.contains("disabledJS")) {
   console.log("Javascript is enabled.");
+  /* Remove Node from DOM if JS is available */
   // noJavascript.classList.add("visually-hidden");
   noJavascript.remove();
 }
