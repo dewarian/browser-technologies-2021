1e79513012c6db5025b0541cc5984489fac432f8
dewarian <nathanbommezijn@gmail.com>
2021-03-29 13:55:12 +0200

    :art: Improve structure of base of server

--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const subjects = [
+  "Web App From Scratch",
+  "CSS to the rescue",
+  "Progressive Web Apps",
+  "Browser Technologies",
+  "Human Centred Design",
+];
 
 router.get("/", (req, res) => {
   res.render("index", {
@@ -9,8 +16,10 @@ router.get("/", (req, res) => {
 
 router.post("/subjectOne", (req, res) => {
   const user = { userId: req.body.userId, userName: req.body.userName };
-  res.render("test", {
+  res.render("subjectOne", {
+    pageTitle: "Minor Web Survey",
     data: user,
+    subject: subjects[0],
   });
 });
 
